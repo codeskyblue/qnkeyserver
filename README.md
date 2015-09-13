fedf7c8183e80aedd209a777dcd22f3973efbe35
shengxiang <hzsunshx@corp.netease.com>
2015-09-13 19:38:43 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # qnkeyserver
 qiniu key server
+
+Need environ
+
+	QINIU_AK
+	QINIU_SK
+	APP_TOKEN
+
+This repo is used by [gorelease](https://github.com/gorelease/gorelease)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/qiniu/api.v6/conf"
+	"github.com/qiniu/api.v6/rs"
+)
+
+func genUptoken(bucketName string, key string) string {
+	policy := rs.PutPolicy{
+		Scope: bucketName + ":" + key,
+	}
+	policy.Expires = uint32(time.Now().Unix()) + 1800
+	policy.FsizeLimit = 20 << 20 // 20M
+	return policy.Token(nil)
+}
+
+func init() {
+	conf.ACCESS_KEY = os.Getenv("QINIU_AK")
+	conf.SECRET_KEY = os.Getenv("QINIU_SK")
+}
+
+func main() {
+	http.HandleFunc("/uptoken", func(w http.ResponseWriter, r *http.Request) {
+		privateToken := r.FormValue("private_token")
+		log.Println(privateToken)
+		scope := r.FormValue("scope")
+		key := r.FormValue("key")
+		if privateToken == os.Getenv("APP_TOKEN") {
+			io.WriteString(w, genUptoken(scope, key))
+			return
+		}
+		http.Error(w, "auth denied", 500)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Qiniu Key Server, request /uptoken to get uptoken")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+	log.Println("Listen port", port)
+	http.ListenAndServe(":"+port, nil)
+}
